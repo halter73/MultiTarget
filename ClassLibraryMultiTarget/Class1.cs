74c9d938a52a2e75b71b03b5812bf5f08ecb85d5
Stephen Halter <halter73@gmail.com>
2022-04-25 10:49:06 -0700

    Initial commit

---
+++ ClassLibraryMultiTarget/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ClassLibraryMultiTarget
+{
+    public static class Class1
+    {
+#if NETSTANDARD2_0_OR_GREATER
+        public static string TargetFramework { get; } = "netstandard2.0";
+#elif NETCOREAPP3_1_OR_GREATER
+        public static string TargetFramework { get; } = "netcoreapp3.1";
+#endif
+    }
+}
---
+++ ConsoleAppCore31/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ClassLibraryMultiTarget;
+
+namespace ConsoleAppCore31
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prints: "netcoreapp3.1"
+            Console.WriteLine(Class1.TargetFramework);
+        }
+    }
+}
